d56ace74da8544db33ad31ae19643178edea751d
delpiter <gioele.foschi.14@gmail.com>
2025-11-25 15:58:15 +0000

    changed some colors

--- app/www/index.php
+++ app/www/index.php
@@ -1,6 +1,7 @@
 <?php 
 require_once 'bootstrap.php';
 require_once rootPath('components', 'Title.php');
+require_once rootPath('components', 'Card.php');
 ?>
 
 <!DOCTYPE html>
@@ -9,11 +10,13 @@ require_once rootPath('components', 'Title.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
     <title>UniCook</title>
 </head>
 
 <body>
-    <?= Title('Hello, World!') ?>
+    <!-- <?= Title('Hello, World!') ?> -->
+    <?= Card() ?>
 </body>
 
 </html>
