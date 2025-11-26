d8814d903286556b768c0583273c74b17ac7e06f
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-11-26 15:12:59 +0000

    bootstrap: require entire directories recursively

--- app/www/bootstrap.php
+++ app/www/bootstrap.php
@@ -1,5 +1,23 @@
 <?php
 session_start();
-require_once 'lib/utils.php';
-require_once 'lib/components.php';
+
+function requireDir(string $dirPath): void {
+    // require_once recursively all PHP files in the given directory
+    $files = scandir($dirPath);
+    foreach ($files as $file) {
+        if ($file === '.' || $file === '..') {
+            continue;
+        }
+        $fullPath = $dirPath . DIRECTORY_SEPARATOR . $file;
+        if (is_dir($fullPath)) {
+            requireDir($fullPath);
+        } elseif (is_file($fullPath) && pathinfo($fullPath, PATHINFO_EXTENSION) === 'php') {
+            require_once $fullPath;
+        }
+    }
+}
+
+requireDir('core'); // core utilities
+requireDir('lib'); // library code
+
 ?>
