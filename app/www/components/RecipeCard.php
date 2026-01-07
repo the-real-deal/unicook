eb09562d6ef3d2be85b83528bdcf61bdfed750c3
delpiter <gioele.foschi.14@gmail.com>
2026-01-07 22:45:50 +0000

    Home Page
    Added the homepage with static card components. Modified: border color, and footer border top

---
+++ app/www/components/RecipeCard.php
@@ -0,0 +1,56 @@
+<?php
+require_once "{$_SERVER['DOCUMENT_ROOT']}/bootstrap.php";
+
+function RecipeCard(string $id, string $recipeTitle, array $tags, int $timeRequired, string $cost) {
+?>
+<div class="p-0 col-sm-12 col-md-6 col-lg-3 d-flex justify-content-center recipe-card">
+    <article class="my-2 pb-2">
+        <div>
+            <img class="card-img-top img-fluid" src="/assets/penne.tmp.jpg" alt="">
+        </div>
+        <h3><?php echo $recipeTitle ?></h3>
+        <ul>
+            <?php 
+                foreach($tags as $tag){
+            ?>
+            <li>
+                <?php echo $tag ?>
+            </li>
+            <?php 
+                }
+            ?>
+        </ul>
+        <div class="d-flex">
+            <div class="pe-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-clock-history" viewBox="0 0 16 16">
+                    <path
+                        d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022zm2.004.45a7 7 0 0 0-.985-.299l.219-.976q.576.129 1.126.342zm1.37.71a7 7 0 0 0-.439-.27l.493-.87a8 8 0 0 1 .979.654l-.615.789a7 7 0 0 0-.418-.302zm1.834 1.79a7 7 0 0 0-.653-.796l.724-.69q.406.429.747.91zm.744 1.352a7 7 0 0 0-.214-.468l.893-.45a8 8 0 0 1 .45 1.088l-.95.313a7 7 0 0 0-.179-.483m.53 2.507a7 7 0 0 0-.1-1.025l.985-.17q.1.58.116 1.17zm-.131 1.538q.05-.254.081-.51l.993.123a8 8 0 0 1-.23 1.155l-.964-.267q.069-.247.12-.501m-.952 2.379q.276-.436.486-.908l.914.405q-.24.54-.555 1.038zm-.964 1.205q.183-.183.35-.378l.758.653a8 8 0 0 1-.401.432z" />
+                    <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z" />
+                    <path
+                        d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5" />
+                </svg>
+                <span aria-label="Time Required"><?php echo $timeRequired ?> min</span>
+            </div>
+            <div class="pe-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+                    fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                    stroke-linejoin="round" class="lucide lucide-dollar-sign w-8 h-8" aria-hidden="true">
+                    <line x1="12" x2="12" y1="2" y2="22"></line>
+                    <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
+                </svg>
+                <span aria-label="Cost"><?php echo $cost ?></span>
+            </div>
+        </div>
+        <a>More...</a>
+        <div class="d-flex justify-content-center align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-bookmark" viewBox="0 0 16 16">
+                <path
+                    d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z">
+                </path>
+            </svg>
+        </div>
+    </article>
+</div>
+<?php } ?>
--- app/www/home/index.php
+++ app/www/home/index.php
@@ -1,25 +1,171 @@
 <?php
 require_once "{$_SERVER['DOCUMENT_ROOT']}/bootstrap.php";
 require_once "components/PageOpening.php";
-require_once "components/HTMLHeader.php";
-require_once "components/FileInput.php";
+require_once "components/Navbar.php";
+require_once "components/Footer.php";
+require_once "components/RecipeCard.php";
 
-PageOpening("Home");
+PageOpening("Home",["style.css"]);
 ?>
 <body>
-    <?= HTMLHeader("Hello, World") ?>
-    <form
-        id="testForm"
-        action="/api/files/upload.php"
-        method="post"
-        enctype="multipart/form-data">
-        Select image to upload:
-        <?= FileInput("fileToUpload", FileType::Image) ?>
-        <br />
-        <input type="submit" value="Upload Image" />
-    </form>
-    <br/>
-    <pre><code id="uploadedImageMetadata">No file uploaded</code></pre>
-    <img id="uploadedImage" alt="uploaded image" />
-    <script type="module" src="main.js"></script>
+    <?php Navbar(); ?>
+    <main class="container-fluid p-0 overflow-x-hidden" id="home-page">
+        <div><!-- Presentational Purposes Only -->
+            <header class="row pt-5 px-5">
+                <div class="col-md-12 col-lg-6 p-0 pt-4 pe-0 pe-lg-4">
+                    <h1>Easy Recipes for Student Life</h1>
+                    <p>Simple, affordable, and quick recipes designed for college students living away from home. Start
+                        cooking with confidence
+                        today!
+                    </p>
+                    <form class="w-100">
+                        <!-- SEARCH BAR -->
+                        <div class=" d-flex align-items-center mb-5 w-100">
+                            <label for="search_bar" hidden>Text input for search purpose</label>
+                            <input id="search_bar" type="search" placeholder="Search Recipes..."
+                                class="me-2 w-75" />
+                            <label for="btn_search" hidden>Button to start the search</label>
+                            <input id="btn_search" type="submit" class="px-4 w-auto" value="Search" />
+                        </div>
+                        <div>
+
+                        </div>
+                    </form>
+                    <form>
+                        <label for="btn_random" hidden>Random Recipe</label>
+                        <input id="btn_random" type="submit" value="Random Recipe" class="py-1">
+                    </form>
+                </div>
+                <div class="d-sm-none d-lg-flex col-lg-6 justify-content-center align-content-center">
+                    <div>
+                        <img src="https://images.unsplash.com/photo-1671725501632-3980b640f420?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjb2xsZWdlJTIwc3R1ZGVudCUyMGNvb2tpbmclMjBraXRjaGVufGVufDF8fHx8MTc2MzEyMzM5NXww&ixlib=rb-4.1.0&q=80&w=1080"
+                            alt="Student cooking">
+                    </div>
+                </div>
+            </header>
+        </div>
+        <section class="row">
+            <h2>Featured Recipes</h2>
+            <p>Handpicked favorites for busy students</p>
+            
+            <?php RecipeCard("1", "Recipe Title#1", ["Tag#1", "Tag#2", "Tag#3"], 20, "Medium") ?>
+            <?php RecipeCard("2", "Recipe Title#2", ["Tag#1", "Tag#2", "Tag#3"], 20, "Medium") ?>
+            <?php RecipeCard("3", "Recipe Title#3", ["Tag#1", "Tag#2", "Tag#3"], 20, "Medium") ?>
+            <?php RecipeCard("4", "Recipe Title#4", ["Tag#1", "Tag#2", "Tag#3"], 20, "Medium") ?>
+        </section>
+
+        <!-- Categories -->
+        <section>
+            <h2>Popular Categories</h2>
+            <p>Find recipes that fit your needs</p>
+            <ul class="row text-center px-0">
+                <li class="col-sm-6 col-md-4 col-lg-2 category-card py-1">
+                    <!-- href="http://www.google.it" -->
+                    <a class="my-1">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <!-- Vegan -->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="lucide lucide-leaf w-8 h-8" aria-hidden="true">
+                                <path
+                                    d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z">
+                                </path>
+                                <path d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12"></path>
+                            </svg>
+                        </div>
+                        Vegan
+                        <p>34 Recipes</p>
+                    </a>
+                </li>
+                <li class="col-sm-6 col-md-4 col-lg-2 category-card py-1">
+                    <a class="my-1">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <!-- Moon -->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="lucide lucide-moon w-8 h-8" aria-hidden="true">
+                                <path
+                                    d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401">
+                                </path>
+                            </svg>
+                        </div>
+                        Night Snacks
+                        <p>34 Recipes</p>
+                    </a>
+                </li>
+                <li class="col-sm-6 col-md-4 col-lg-2 category-card py-1">
+                    <a class="my-1">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <!-- International -->
+                            <svg xmlns="http://www.w3.org/2000/svg" height viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="lucide lucide-globe w-8 h-8" aria-hidden="true">
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path>
+                                <path d="M2 12h20"></path>
+                            </svg>
+                        </div>
+                        International
+                        <p>34 Recipes</p>
+                    </a>
+                </li>
+                <li class="col-sm-6 col-md-4 col-lg-2 category-card py-1">
+                    <a class="my-1">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <!-- Low Ingredients -->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="lucide lucide-cookie w-8 h-8" aria-hidden="true">
+                                <path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"></path>
+                                <path d="M8.5 8.5v.01"></path>
+                                <path d="M16 15.5v.01"></path>
+                                <path d="M12 12v.01"></path>
+                                <path d="M11 17v.01"></path>
+                                <path d="M7 14v.01"></path>
+                            </svg>
+                        </div>
+                        Few Ingredients
+                        <p>34 Recipes</p>
+                    </a>
+                </li>
+                <li class="col-sm-6 col-md-4 col-lg-2 category-card py-1">
+                    <a class="my-1">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <!-- Quick  -->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="lucide lucide-zap w-8 h-8" aria-hidden="true">
+                                <path
+                                    d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z">
+                                </path>
+                            </svg>
+                        </div>
+                        Quick Meals
+                        <p>34 Recipes</p>
+                    </a>
+                </li>
+                <li class="col-sm-6 col-md-4 col-lg-2 category-card py-1">
+                    <a class="my-1">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <!-- Budget -->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="lucide lucide-dollar-sign w-8 h-8" aria-hidden="true">
+                                <line x1="12" x2="12" y1="2" y2="22"></line>
+                                <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
+                            </svg>
+                        </div>
+                        Budget-Friendly
+                        <p>34 Recipes</p>
+                    </a>
+                </li>
+            </ul>
+        </section>
+    </main>
+
+    <?php Footer();?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
 </body>
