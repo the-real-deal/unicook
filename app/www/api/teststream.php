48bf5f3dc78b8bcd244099ac2a4db5f3b081ac83
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-01-10 20:12:49 +0000

    basic stream working

---
+++ app/www/api/teststream.php
@@ -0,0 +1,29 @@
+<?php
+require_once "{$_SERVER['DOCUMENT_ROOT']}/bootstrap.php";
+require_once "lib/core/api.php";
+require_once "lib/utils.php";
+
+$server = new ApiServer();
+
+define("MISTRAL_API_URL", "https://api.mistral.ai/v1/chat/completions");
+// clear api key for demo purposes
+define("MISTRAL_API_KEY", envValue("MISTRAL_API_KEY") || "IhRvclzbzEpKU2WbHmbALC45M0uEo3wu");
+define("MISTRAL_AGENT_ID", envValue("MISTRAL_AGENT_ID") || "ag_019b12d4995071a98c64db6fd041d99e");
+
+$server->addEndpoint(HTTPMethod::POST, function ($req, $res) {
+    $res->setHeader(HTTPHeader::ContentType, MimeType::TextEventStream);
+    // https://itsrav.dev/articles/streaming-http-response-in-php-to-turn-long-running-process-into-realtime-experience
+    set_time_limit(0); // max execution time unlimited
+    ob_implicit_flush(1); // automatically flush every output
+
+    $prompt = $_POST["prompt"];
+    
+    for ($i=0; $i < 10; $i++) { 
+        sleep(1);
+        echo "#$i $prompt\n";
+    }
+});
+
+$server->respond();
+
+?>
---
+++ app/www/lib/core/http.php
@@ -0,0 +1,32 @@
+<?php
+require_once "{$_SERVER['DOCUMENT_ROOT']}/bootstrap.php";
+require_once "lib/core/mime.php";
+
+enum HTTPHeader: string {
+    case ContentType = "Content-Type";
+    case ContentLength = "Content-Length";
+    case Location = "Location";
+
+    public function checkData(mixed $data): bool {
+        return match ($this) {
+            self::ContentType => $data instanceof MimeType,
+            self::ContentLength => is_int($data) && $data >= 0,
+            default => true,
+        };
+    }
+}
+
+enum HTTPCode: int {
+    case NotFound = 404;
+    case BadRequest = 400;
+    case MethodNotAllowed = 405;
+    case InternalServerError = 500;
+    case OK = 200;
+}
+
+enum HTTPMethod: string {
+    case GET = "GET";
+    case POST = "POST";
+}
+
+?>
