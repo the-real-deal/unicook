fae683217f55ce2f93c668cff9f5b0cabc42944b
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-01-10 21:44:09 +0000

    simpler streaming

--- app/www/api/teststream.php
+++ app/www/api/teststream.php
@@ -11,7 +11,7 @@ define("MISTRAL_API_KEY", envValue("MISTRAL_API_KEY") || "nh5cxczbW9RHViDA6WhFnM
 define("MISTRAL_AGENT_ID", envValue("MISTRAL_AGENT_ID") || "ag_019b12d4995071a98c64db6fd041d99e");
 
 $server->addEndpoint(HTTPMethod::POST, function ($req, $res) {
-    $res->setHeader(HTTPHeader::ContentType, MimeType::TextEventStream);
+    $res->setHeader(HTTPHeader::ContentType, MimeType::PlainText);
     // https://itsrav.dev/articles/streaming-http-response-in-php-to-turn-long-running-process-into-realtime-experience
     set_time_limit(0); // max execution time unlimited
     ob_implicit_flush(1); // automatically flush every output
