1bfdac015af54e5bc3c54e85ac59dd3241846274
delpiter <gioele.foschi.14@gmail.com>
2026-01-11 23:31:20 +0000

    fixed cut svg

--- app/www/components/RecipeCard.php
+++ app/www/components/RecipeCard.php
@@ -44,7 +44,7 @@ function RecipeCard(string $id, string $recipeTitle, array $tags, int $timeRequi
         </div>
         <a href="/singleRecipe" >More...</a>
         <button id="btn-<?php echo $id?>" class="d-flex justify-content-center align-items-center" onclick="saveRecipe('<?php echo 'btn-'.$id ?>')" type="button" title="tmp">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="transparent" stroke="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="transparent" stroke="currentColor" class="bi bi-bookmark-fill" viewBox="-1 -1 18 18">
                 <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
             </svg>
         </button>
