d7a86bed685404cd7ee4a1480e190f48b4c10224
delpiter <gioele.foschi.14@gmail.com>
2026-01-20 09:54:16 +0000

    Added editing buttons to single recipe

--- app/www/singleRecipe/index.php
+++ app/www/singleRecipe/index.php
@@ -11,6 +11,7 @@ require_once "lib/recipes.php";
 
 $db = Database::connectDefault();
 $login = LoginSession::autoLogin($db);
+$recipe = false;
 
 $server = new ApiServer();
 
@@ -73,7 +74,10 @@ if($instructions===false){
                 <div class="my-4 position-relative">
                     <img src="/api/recipes/image.php?recipeId=<?= $recipe->id ?>"
                         class="object-fit-cover w-100" alt="Students cooking together"/>
-                    <div class="d-flex">
+                    <?php 
+                        if($login !== false && ($login->user->isAdmin || $recipe->userId === $login->user->id)){
+                    ?>
+                    <div class="d-flex control-buttons">
                         <a href="" title="edit recipe" class="d-flex justify-content-center align-items-center p-2">
                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil flex-shrink-0" viewBox="-1 -1 18 18">
                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
@@ -84,14 +88,16 @@ if($instructions===false){
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                             </svg>
-                        </button>   
+                        </button>
                         <button id="save_btn_1" class="d-flex justify-content-center align-items-center p-2 ms-2" onclick="saveRecipe('save_btn_1','<?= $recipe->id?>')" type="button" title="<?= $saved?"remove from saved":"save" ?>">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="<?= $saved?"currentColor":"transparent" ?>" stroke="currentColor" class="bi bi-bookmark-fill flex-shrink-0" viewBox="-1 -1 18 18">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="transparent" stroke="currentColor" class="bi bi-bookmark-fill flex-shrink-0" viewBox="-1 -1 18 18">
                                 <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
                             </svg>
                         </button>   
                     </div>
-                    
+                    <?php 
+                        }
+                    ?>
                 </div>
                 <ul id="tagsUl" class="d-flex px-5">
                     <?php 
