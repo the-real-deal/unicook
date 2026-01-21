69cb2f9ebc7fcf7f1f04d6107a761714518637b7
delpiter <gioele.foschi.14@gmail.com>
2026-01-21 13:03:08 +0000

    Fixed Accessibility problem and styled checkbox

--- app/www/singleRecipe/index.php
+++ app/www/singleRecipe/index.php
@@ -119,10 +119,10 @@ if($instructions===false){
                     ?>
                 </ul>
 
-                <div>
+                <header>
                     <h1 class="mb-4"><?= $recipe->title ?></h1>
                     <p class="d-flex flex-wrap"><?= $recipe->description ?></p>
-                </div>
+                </header>
 
                 <ul class="row d-flex mt-4 w-100 m-0 ps-0">
                     <li class="text-center col-6 col-lg-3">
@@ -189,7 +189,7 @@ if($instructions===false){
                                 $i = 0;
                                 foreach ($ingredients as $ingredient) { 
                                     ?>
-                                <li class="d-flex flex-wrap my-3">
+                                <li class="d-flex my-3 align-items-center">
                                     <input id="ingredients<?= $i ?>" type="checkbox" class="me-2" />
                                     <label for="ingredients<?= $i ?>">
                                         <?= $ingredient->name." ".$ingredient->quantity ?>
@@ -232,38 +232,38 @@ if($instructions===false){
                             <?php } else { ?>
                             <form class="p-3 mb-3">
                                 <fieldset class="mb-3">
-                                    <legend hidden>Star rating</legend>
-                                    <label class="mb-2">Your Rating</label>
+                                    <legend>Your Rating</legend>
+                                    <!-- <label class="mb-2" for="rating">Your Rating</label> -->
                                     <div class="stars">
-                                        <input type="radio" name="rating" id="r1" value="1" hidden/>
+                                        <input type="radio" name="rating" id="r1" value="1"/>
                                         <label for="r1" class="pe-1">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="-1 -1 18 18">
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                             </svg>
                                             <span hidden>1 star</span>
                                         </label>
-                                        <input type="radio" name="rating" id="r2" value="2" hidden/>
+                                        <input type="radio" name="rating" id="r2" value="2"/>
                                         <label for="r2" class="pe-1">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="-1 -1 18 18">
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                             </svg>
                                             <span hidden>2 star</span>
                                         </label>
-                                        <input type="radio" name="rating" id="r3" value="3" hidden/>
+                                        <input type="radio" name="rating" id="r3" value="3"/>
                                         <label for="r3" class="pe-1">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="-1 -1 18 18">
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                             </svg>
                                             <span hidden>3 star</span>
                                         </label>
-                                        <input type="radio" name="rating" id="r4" value="4" hidden/>
+                                        <input type="radio" name="rating" id="r4" value="4"/>
                                         <label for="r4" class="pe-1">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="-1 -1 18 18">
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                             </svg>
                                             <span hidden>4 star</span>
                                         </label>
-                                        <input type="radio" name="rating" id="r5" value="5" hidden/>
+                                        <input type="radio" name="rating" id="r5" value="5" checked/>
                                         <label for="r5" class="pe-1">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="-1 -1 18 18">
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
