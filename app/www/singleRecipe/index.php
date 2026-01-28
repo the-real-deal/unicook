589996a8b1f7fd18d0be21b16ca9df2a85ed178f
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-01-28 16:41:14 +0000

    shhhhh

--- app/www/singleRecipe/index.php
+++ app/www/singleRecipe/index.php
@@ -82,10 +82,10 @@ if($instructions===false){
                 <div class="my-4 position-relative">
                     <img src="/api/recipes/image.php?recipeId=<?= $recipe->id ?>"
                         class="object-fit-cover w-100" alt="Students cooking together"/>
-                    <?php 
-                        if($login !== false && ($login->user->isAdmin || $recipe->userId === $login->user->id)){
-                    ?>
                     <div class="d-flex control-buttons">
+                        <?php 
+                        if($login !== false && ($login->user->isAdmin || $recipe->userId === $login->user->id)){
+                        ?>
                         <a href="/createRecipe?recipeId=<?= $recipe->id ?>" title="edit recipe" class="d-flex justify-content-center align-items-center p-2">
                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil flex-shrink-0" viewBox="-1 -1 18 18">
                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
@@ -97,15 +97,13 @@ if($instructions===false){
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                             </svg>
                         </button>
+                        <?php } ?>
                         <button id="save_btn_1" class="d-flex justify-content-center align-items-center p-2 ms-2" type="button" title="<?=  $recipe->isSavedFrom($db, $login->user)?"remove from saved":"save" ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="transparent" stroke="currentColor" class="bi bi-bookmark-fill flex-shrink-0" viewBox="-1 -1 18 18">
                                 <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
                             </svg>
                         </button>   
                     </div>
-                    <?php 
-                        }
-                    ?>
                 </div>
                 <ul id="tagsUl" class="d-flex flex-wrap px-3 px-md-5">
                     <?php 
